c3edb0a2a9a497cead3e4b5b53b3dcd78e5d0100
lucasphill <lucaspscp@outlook.com>
2025-03-15 20:48:35 -0300

    Base da pagina de informações e cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HeroUIProvider } from "@heroui/react";
+import { ToastProvider } from "@heroui/toast";
 import "./index.css";
 import App from "./App.tsx";
-
-import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+      <ToastProvider />
+      <main className="w-screen h-screen flex-col justify-center purple-dark text-foreground bg-background">
+        <App />
+      </main>
     </HeroUIProvider>
   </StrictMode>
 );
